8d2b346191c17252f150807851a7a343b2cd8104
Olexandr Zabolotniy <w.zabolotniy.olexandr@gmail.com>
2023-01-05 00:14:11 +0200

    Add file with home work 1

---
+++ Udemy_HW_1.py
@@ -0,0 +1,21 @@
+# 1. Create a list with 5 elements of different types
+first_list = ['Python', True, 555, 10.5, {'sum': 100}]
+
+# 2. Delete the third element
+first_list.pop(2)
+
+# 3. Output the length of the list to the terminal
+print(len(first_list))
+
+# 4. Change the order of elements in the list
+first_list.reverse()
+print(first_list)
+
+# 5. Create another list with two items
+second_list = ['Java', 'PHP']
+
+# 6. Expand the first list by elements of the second list
+combined_list = first_list + second_list
+
+# 7. Output an extended list of 6 items to the discrimination
+print(combined_list)
