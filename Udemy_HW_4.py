e30a9f0e365688b37726454c2c70234ab6c98f51
exorive <w.zabolotniy.olexandr@gmail.com>
2023-01-07 13:30:28 +0200

    Add new file with home work 4

---
+++ Udemy_HW_4.py
@@ -0,0 +1,17 @@
+first_tuple = ('apple', True, 500, 1.5)
+users = (
+    {
+        'user_id': 134,
+        'user_name': 'Alice'
+    },
+    {
+        'user_id': 831,
+        'user_name': 'Bob'
+    }
+)
+
+combined_tuple = first_tuple + users
+
+print(combined_tuple)
+
+
