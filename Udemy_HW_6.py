155af5c31192cb1a927b9e30c940c9bb7ea6865d
exorive <w.zabolotniy.olexandr@gmail.com>
2023-01-10 01:29:35 +0200

    Add file with home work 6

---
+++ Udemy_HW_6.py
@@ -0,0 +1,16 @@
+# 1.Create a list with product names 
+products = ['apple', 'banana', 'lime']
+
+# 2. Create a list with prices
+prices = [25, 70, 55]
+
+# 3. Combine lists
+combined_zip = zip(products, prices)
+
+# 4. Конвертировать в список
+combined_zip_list = list(combined_zip)
+
+# 5. Output the result to the terminal
+print(combined_zip_list)
+# [('apple', 25), ('banana', 70), ('lime', 55)]
+
