8616d2d2de2e427c66ba3bcbeb6343d158126819
exorive <w.zabolotniy.olexandr@gmail.com>
2023-01-10 01:34:21 +0200

    Translate the fourth point of the task into English

--- Udemy_HW_6.py
+++ Udemy_HW_6.py
@@ -7,7 +7,7 @@ prices = [25, 70, 55]
 # 3. Combine lists
 combined_zip = zip(products, prices)
 
-# 4. Конвертировать в список
+# 4. Convert to a list
 combined_zip_list = list(combined_zip)
 
 # 5. Output the result to the terminal
