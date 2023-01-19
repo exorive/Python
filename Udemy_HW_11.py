0741a7157c0407bb1422e3265791b692c5668331
Olexandr Zabolotniy <w.zabolotniy.olexandr@gmail.com>
2023-01-19 16:01:43 +0200

    Add file with home work 11

---
+++ Udemy_HW_11.py
@@ -0,0 +1,13 @@
+first_dict = {
+    'apple': 100,
+    'banana': 500,
+    'plum': 150,
+}
+
+second_dict = {
+    'apple': 100,
+    'banana': 500,
+    'plum': 150,
+}
+
+first_dict and second_dict and print("These dictionaries are the same")
